f76e5ee22eee09abc7fb6a97b67ca6f30c378e8a
votanak <v0tanak@yandex.ru>
2025-01-05 19:30:19 +0300

    first commit

---
+++ background.js
@@ -0,0 +1,61 @@
+﻿chrome.action.onClicked.addListener(async () => {
+  const bitrixTabId = await chrome.tabs.query(
+    { url: 'https://labelectro.bitrix24.ru/marketplace/app/22/' },
+    function (tabs) {
+      const tabId = tabs[0].id;
+      console.log(tabs[0]);
+
+      chrome.scripting.executeScript(
+        {
+          target: { tabId },
+          func: function () {
+            const iframeDoc = document.querySelector(
+              'iframe[src="/marketplace/app/22/?IFRAME=Y&IFRAME_TYPE=SIDE_SLIDER"]',
+            ).contentDocument;
+            console.log('iframe: ', iframeDoc);
+
+            const input = iframeDoc.querySelector('input[name=AUTH_ID]');
+            return input ? input.value : 'Элемент не найден';
+          },
+        },
+        async (results) => {
+          if (chrome.runtime.lastError) {
+            console.error('Ошибка:', chrome.runtime.lastError.message);
+          } else {
+            // Открываем локальную страницу target.html
+            const url = chrome.runtime.getURL('target.html');
+
+            // Создаем новую вкладку с target.html
+            const targetTab = await chrome.tabs.create({ url });
+
+            // Ждем загрузки страницы и отправляем сообщение
+            chrome.tabs.onUpdated.addListener(function listener(
+              tabId,
+              changeInfo,
+            ) {
+              if (tabId === targetTab.id && changeInfo.status === 'complete') {
+                // Сообщение отправляется только после загрузки target.html
+                chrome.tabs.sendMessage(
+                  targetTab.id,
+                  { text: results[0].result },
+                  (response) => {
+                    if (chrome.runtime.lastError) {
+                      console.error(
+                        'Ошибка отправки сообщения:',
+                        chrome.runtime.lastError.message,
+                      );
+                    } else {
+                      console.log('Ответ от target.js:', response);
+                    }
+                  },
+                );
+
+                chrome.tabs.onUpdated.removeListener(listener); // Удаляем слушатель
+              }
+            });
+          }
+        },
+      );
+    },
+  );
+});
---
+++ target.js
@@ -0,0 +1,47 @@
+const zapros = async (authId) => {
+  try {
+    const response = await fetch(
+      'https://integrations.wazzup24.com/bitrix/connect?DOMAIN=labelectro.bitrix24.ru&PROTOCOL=1&LANG=ru&APP_SID=cd27c026457240cfe9fd0cb79feba342',
+      {
+        method: 'POST',
+        headers: {
+          accept:
+            'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+          'content-type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams({
+          AUTH_ID: authId,
+          AUTH_EXPIRES: '3333600',
+          REFRESH_ID:
+            '1b9a9b6700220330005190e400000008000007998b399dccf8578b00c1a71408c98eff',
+          member_id: 'dbfea47052b504cb009720d53658bfaa',
+          status: 'F',
+          PLACEMENT: 'DEFAULT',
+          PLACEMENT_OPTIONS: JSON.stringify({
+            IFRAME: 'Y',
+            IFRAME_TYPE: 'SIDE_SLIDER',
+            any: '22/',
+          }),
+        }),
+      },
+    );
+    console.log(response);
+
+    if (!response.ok) {
+      throw new Error(`Ошибка HTTP: ${response.status}`);
+    }
+
+    const html = await response.text();
+    responseContainer.innerHTML += html;
+  } catch (error) {
+    responseContainer.innerHTML += `<p>Ошибка: ${error.message}</p>`;
+  }
+};
+
+// Обработка сообщений от фонового скрипта
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  console.log('Сообщение получено на target.html:', message.text);
+  // Отправляем сообщение через window.postMessage для демонстрации
+  window.postMessage({ text: message.text }, '*');
+  zapros(message.text);
+});
